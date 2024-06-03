c398fa6d163a39ecfd5e3b33cecd9c4908380581
“tyraiel” <tyraiel@gmail.com>
2024-06-03 19:42:20 -0300

    sin titulos

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,5 @@
 
-import { CartCounter } from "@/app/shopping-cart";
-import { title } from "process";
-import { useState } from "react";
-import ChatGPTHome from '../../../pages/abmcontadores';
+
 import CounterDashboard from '@/pages/dashboard/counterchat'
 
 export const metadata = {
@@ -18,9 +15,7 @@ export default function CounterPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1>Producto en el carrito</h1>
-      <CartCounter value={20} />
         <CounterDashboard />
-        <h1>Productos en el carrito</h1>
     </div>
   );
 }
